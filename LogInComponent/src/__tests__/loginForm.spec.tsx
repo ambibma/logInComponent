77435292c3cffeee2db5ebdada85a99e7ff81749
ambi hidalgo <ambio.pk@gmail.com>
2024-05-06 21:33:32 -0700

    Add Test 2 does not call onLogin function when clicking the Login button with empty inputs

--- LogInComponent/src/__tests__/loginForm.spec.tsx
+++ LogInComponent/src/__tests__/loginForm.spec.tsx
@@ -8,21 +8,9 @@ describe('Login Form component', () => {
     expect(getByPlaceholderText('Username')).toBeInTheDocument();
     expect(getByPlaceholderText('Password')).toBeInTheDocument();
     expect(getByRole('button', { name: 'Login' })).toBeInTheDocument();
-  })
-  it('calls onLogin function when clicking Login button with valid credentials', () => {
-    const onLoginMock = jest.fn();
-    const { getByRole, getByPlaceholderText } = render(<LoginForm onLogin={onLoginMock} />);
-    const usernameInput = getByPlaceholderText('Username');
-    const passwordInput = getByPlaceholderText('Password');
-    const loginButton = getByRole('button', { name: 'Login' });
-    
-    fireEvent.change(usernameInput, { target: { value: 'testuser' } });
-    fireEvent.change(passwordInput, { target: { value: 'testpassword' } });
-    fireEvent.click(loginButton);
-
-    expect(onLoginMock).toHaveBeenCalled();
-
+  
   });
+  
   it('does not call onLogin function when clicking the Login button with empty inputs', () => {
     const onLoginMock = jest.fn();
     const { getByRole } = render(<LoginForm onLogin={onLoginMock} />);
