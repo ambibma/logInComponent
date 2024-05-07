a063b19477cc21cab7e2a1f9189f5af1b072882f
ambi hidalgo <ambio.pk@gmail.com>
2024-05-06 20:56:06 -0700

    Add Test 1 LoginPage renders inputs and button
    Add Test 2 calls onLogin function when clicking  Login button with valid credentials'
    
    Add Test 3 does not call onLogin function when clicking the Login button with empty inputs

--- LogInComponent/src/__tests__/loginForm.spec.tsx
+++ LogInComponent/src/__tests__/loginForm.spec.tsx
@@ -1,3 +1,37 @@
-test('demo', () => {
-  expect(true).toBe(true)
-})
+import { render,fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/'; 
+import LoginForm from '../components/loginForm';
+
+describe('Login Form component', () => {
+  it('renders username and password fields with a login button', ()=>{
+    const { getByPlaceholderText, getByRole } = render(<LoginForm onLogin={() => {}} />);
+    expect(getByPlaceholderText('Username')).toBeInTheDocument();
+    expect(getByPlaceholderText('Password')).toBeInTheDocument();
+    expect(getByRole('button', { name: 'Login' })).toBeInTheDocument();
+  })
+  it('calls onLogin function when clicking Login button with valid credentials', () => {
+    const onLoginMock = jest.fn();
+    const { getByRole, getByPlaceholderText } = render(<LoginForm onLogin={onLoginMock} />);
+    const usernameInput = getByPlaceholderText('Username');
+    const passwordInput = getByPlaceholderText('Password');
+    const loginButton = getByRole('button', { name: 'Login' });
+    
+    fireEvent.change(usernameInput, { target: { value: 'testuser' } });
+    fireEvent.change(passwordInput, { target: { value: 'testpassword' } });
+    fireEvent.click(loginButton);
+
+    expect(onLoginMock).toHaveBeenCalled();
+
+  });
+  it('does not call onLogin function when clicking the Login button with empty inputs', () => {
+    const onLoginMock = jest.fn();
+    const { getByRole } = render(<LoginForm onLogin={onLoginMock} />);
+    const loginButton = getByRole('button', {name: 'Login'});
+
+    fireEvent.click(loginButton);
+
+    expect(onLoginMock).not.toHaveBeenCalled();
+
+  });
+
+});
