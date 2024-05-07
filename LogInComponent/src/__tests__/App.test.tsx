81c5e246473c7a2e939982837226dcba2b3622f8
ambi hidalgo <ambio.pk@gmail.com>
2024-05-06 20:17:31 -0700

    Test 1 App Renders on page

--- LogInComponent/src/__tests__/App.test.tsx
+++ LogInComponent/src/__tests__/App.test.tsx
@@ -2,11 +2,10 @@ import '@testing-library/jest-dom';
 import {render} from "@testing-library/react";
 import App from "../App";
 
-test('demo', () => {
-  expect(true).toBe(true)
-})
+
 
 test("Renders the Main page", () => {
   render(<App/>)
   expect(true).toBeTruthy()
 })
+
